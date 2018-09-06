61ebedd2c392e4c3a312524f5f09014162273cbc
Justin Obney <justinobney@gmail.com>
2018-09-05 20:55:51 -0500

    chore: cleanup example app

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {Container, Header, Segment} from 'semantic-ui-react';
+import {Container, Header, Segment, Tab} from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
@@ -9,53 +9,59 @@ import SimpleForm from './forms/SimpleForm';
 import ExampleForm from './forms/ExampleForm';
 import SchemaForm from './forms/SchemaForm';
 
+const panes = [
+  {
+    menuItem: 'Simple Form',
+    render: () => (
+      <Tab.Pane attached={false}>
+        <SimpleForm key="default" />
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: 'Simple Form with Data',
+    render: () => (
+      <Tab.Pane attached={false}>
+        <SimpleForm
+          key="with-data"
+          person={{
+            emailAddress: 'jobney@turner-industries.com',
+            firstName: 'Justin',
+            lastName: 'Obney',
+            checkbox: true,
+            radio: 2,
+          }}
+        />
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: 'Render/Child Function Props',
+    render: () => (
+      <Tab.Pane attached={false}>
+        <ExampleForm />
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: 'Schema Form',
+    render: () => (
+      <Tab.Pane attached={false}>
+        <SchemaForm />
+      </Tab.Pane>
+    ),
+  },
+];
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Header as="h1">Formik-Semantic-UI</Header>
-        <Container style={{paddingTop: 50}}>
-          <Header as="h2" attached="top" inverted>
-            Simple Form
-          </Header>
-
-          <Segment attached>
-            <SimpleForm />
-          </Segment>
-        </Container>
-        <Container style={{paddingTop: 50}}>
-          <Header as="h2" attached="top" inverted>
-            Simple Form with Data
-          </Header>
-
-          <Segment attached>
-            <SimpleForm
-              person={{
-                emailAddress: 'jobney@turner-industries.com',
-                firstName: 'Justin',
-                lastName: 'Obney',
-                checkbox: true,
-                radio: 2,
-              }}
-            />
-          </Segment>
-        </Container>
-        <Container style={{paddingTop: 50}}>
-          <Header as="h2" attached="top" inverted>
-            Render/Child Function Props
-          </Header>
-
-          <Segment attached>
-            <ExampleForm />
-          </Segment>
-        </Container>
-        <Container style={{paddingTop: 50}}>
-          <Header as="h2" attached="top" inverted>
-            Schema Form
-          </Header>
-
-          <Segment attached>
-            <SchemaForm />
+        <Container>
+          <br />
+          <Header as="h1">Formik-Semantic-UI</Header>
+          <Segment>
+            <Tab menu={{secondary: true, pointing: true}} panes={panes} />
           </Segment>
         </Container>
       </React.Fragment>
